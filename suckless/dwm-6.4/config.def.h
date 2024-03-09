878a5753754734aa452839c9092549f27a219dc8
Abhra00 <abhramondal9674@gmail.com>
2024-03-10 02:41:24 +0530

    made a script and removed all config.h

--- suckless/dwm-6.4/config.def.h
+++ suckless/dwm-6.4/config.def.h
@@ -152,6 +152,7 @@ ResourcePref resources[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, "-p", "Run:", NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *pmenucmd[]  = { "/home/bugs/myLinuxDotFiles/suckless/dwm-6.4/scripts/pmenu", NULL };
 
 /* volume controls */
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
@@ -170,6 +171,7 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|Mod1Mask,              XK_x, 	   spawn,          {.v = pmenucmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
